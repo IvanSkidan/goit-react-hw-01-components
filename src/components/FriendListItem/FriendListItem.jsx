719ca776625f332724e297e320d1f1d58c60f1a4
Ivan Skidan <vanyalife7@gmail.com>
2023-05-15 10:43:05 +0300

    updated the styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -6,9 +6,7 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
       <span
-        className={css.status}
-        style={{ background: isOnline ? 'green' : 'red' }}
-      ></span>
+        className={`${css.status} ${css[isOnline ? 'green' : 'red']}`}></span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
